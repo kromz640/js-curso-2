d26253ab27407f8674f03783d31e5e21dffeac08
Rolando Roque Kuan <rroquek@msn.com>
2024-01-19 20:06:36 -0600

    Reto2 Oracle One
    Reto de funciones js

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
+let titulo=document.querySelector('h1');
+titulo.innerHTML="Hora de desafio";
 
+
+function clickConsola()
+{
+    alert("Hiciste click en boton consola")
+}
+
+
+function preguntarCiudad()
+{
+    let ciudad=prompt("Escribe una ciudad de brazil");
+    alert(`Estuve en la ciudad de ${ciudad} y me acorde de ti`);
+}
+
+function amoJs()
+{
+    alert("Yo amo JS");
+}
+
+function sumarDosNumeros()
+{
+    let numero1=parseInt(prompt("Escribe el primer numero"));
+    let numero2=parseInt(prompt("Escribe el segundo numero"));
+    let suma=parseInt(numero1+numero2);
+    alert(`La suma es ${suma}`);
+
+}
